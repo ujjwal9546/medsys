2c21984dfebc175cdeb110551a01d5740688f6db
ujjwal9546 <ujjwalkumarray9546@gmail.com>
2026-05-21 22:26:01 +0530

    fix static root for deployment

--- medsys/settings.py
+++ medsys/settings.py
@@ -135,6 +135,7 @@ STATIC_URL='static/'
 STATICFILES_DIRS=[
 BASE_DIR/'static'
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 AUTH_USER_MODEL='accounts.User'
