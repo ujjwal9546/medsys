bf2094261b9ac372029855510d01411291ed2559
ujjwal9546 <ujjwalkumarray9546@gmail.com>
2026-05-21 22:53:43 +0530

    fix allowed hosts for deployment

--- medsys/settings.py
+++ medsys/settings.py
@@ -34,7 +34,11 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "medsys-2mrq.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
