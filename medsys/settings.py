199697e96770f728b4d986492208ef3814ef8297
ujjwal9546 <ujjwalkumarray9546@gmail.com>
2026-05-28 14:10:57 +0530

    postgres setup

--- medsys/settings.py
+++ medsys/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 from dotenv import load_dotenv
-import dj_database_url
+
+
 
 
 load_dotenv()
@@ -92,12 +94,32 @@ WSGI_APPLICATION = 'medsys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
-}
 
+if os.environ.get("DATABASE_URL"):
+
+    DATABASES = {
+
+        'default': dj_database_url.parse(
+
+            os.environ.get("DATABASE_URL")
+
+        )
+
+    }
+
+else:
+
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.sqlite3',
+
+            'NAME': BASE_DIR / 'db.sqlite3',
+
+        }
+
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
